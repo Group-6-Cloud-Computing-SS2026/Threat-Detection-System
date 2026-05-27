bbceed9215f9b303a2dc46ff60f9ae48ed29dca5
javierdesant <j.desantiagosoto@gmail.com>
2026-05-27 18:44:04 +0200

    feat: add HomePage component

---
+++ frontend/src/features/home/HomePage.tsx
@@ -0,0 +1,15 @@
+export default function HomePage() {
+    return (
+        <>
+            <title>Home | Threat Detection System</title>
+            <meta property="og:title" content="Home | Threat Detection System" />
+            <meta
+                name="description"
+                content="Overview dashboard for the Threat Detection System."
+            />
+
+            <section>
+            </section>
+        </>
+    );
+}
