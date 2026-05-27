a9a315363f152445272467d5d34e8e29edc74985
javierdesant <j.desantiagosoto@gmail.com>
2026-05-27 18:44:09 +0200

    feat: add LandingPage component

---
+++ frontend/src/features/landing/LandingPage.tsx
@@ -0,0 +1,15 @@
+export default function LandingPage() {
+    return (
+        <>
+            <title>Threat Detection System</title>
+            <meta property="og:title" content="Threat Detection System" />
+            <meta
+                name="description"
+                content="An edge-computing platform for real-time threat detection and monitoring."
+            />
+
+            <section>
+            </section>
+        </>
+    );
+}
