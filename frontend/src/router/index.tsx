f25ebd6ca4fded95b6a60e6eeed8d638ec881acf
javierdesant <j.desantiagosoto@gmail.com>
2026-05-27 18:44:41 +0200

    feat: add nested routes to router

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,10 +1,42 @@
 import { createBrowserRouter } from "react-router";
-import Root from "./Root";
+import Root from "./Root.tsx";
+import LoginPage from "../features/auth/LoginPage.tsx";
+import ProfilePage from "../features/auth/ProfilePage.tsx";
+import DocsPage from "../features/docs/DocsPage.tsx";
+import HomePage from "../features/home/HomePage.tsx";
+import LandingPage from "../features/landing/LandingPage.tsx";
+import SettingsPage from "../features/settings/SettingsPage.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Root />,
+        Component: Root,
+        children: [
+            {
+                index: true,
+                Component: HomePage,
+            },
+            {
+                path: "landing",
+                Component: LandingPage,
+            },
+            {
+                path: "login",
+                Component: LoginPage,
+            },
+            {
+                path: "settings",
+                Component: SettingsPage,
+            },
+            {
+                path: "profile",
+                Component: ProfilePage,
+            },
+            {
+                path: "docs",
+                Component: DocsPage,
+            },
+        ],
     },
 ]);
 
