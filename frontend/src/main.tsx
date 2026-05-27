ac424fe4732472c123cfbd16cb874fa895c8aac0
javierdesant <j.desantiagosoto@gmail.com>
2026-05-27 15:51:34 +0200

    refactor: extract router to separate module

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,8 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import './index.css'
-import Root from './Root.tsx'
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />
-    }
-]);
+import router from "./router/router.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
