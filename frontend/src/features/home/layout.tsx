b1c86c9ffdfa7935aacb5c899c86b5e0a67cdd70
Javier de Santiago <j.desantiagosoto@gmail.com>
2026-07-02 17:56:24 +0200

    feat(home): add default layout with animation support

---
+++ frontend/src/features/home/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Footer from "../../shared/components/ui/footer.tsx";
+
+export default function DefaultLayout({
+                                          children,
+                                      }: {
+    children: React.ReactNode;
+}) {
+    useEffect(() => {
+        AOS.init({
+            once: true,
+            disable: "phone",
+            duration: 600,
+            easing: "ease-out-sine",
+        });
+    });
+
+    return (
+        <>
+            <main className="relative flex grow flex-col">{children}</main>
+
+            <Footer />
+        </>
+    );
+}
