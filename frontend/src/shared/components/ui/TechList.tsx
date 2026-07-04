05990548ed10492e7301240b92808477843d38fb
Javier de Santiago <j.desantiagosoto@gmail.com>
2026-07-04 18:34:10 +0200

    refactor(footer): modularize team and tech lists with reusable components

---
+++ frontend/src/shared/components/ui/TechList.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import type {TagUrl} from "../../types";
+
+type TechListProps = {
+    technologies: TagUrl[];
+};
+
+const TechList: React.FC<TechListProps> = ({ technologies }) => {
+    return (
+        <ul className="space-y-2 text-sm">
+            {technologies.map((technology, index) => (
+                <li key={index}>
+                    <a
+                        className="text-brand-light-green-200/65 transition hover:text-brand-light-green-500"
+                        href={technology.url.href}
+                    >
+                        {technology.tag}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default TechList;
