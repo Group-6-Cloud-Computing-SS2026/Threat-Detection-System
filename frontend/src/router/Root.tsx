d688e7d0dfa0bd7d99204ab3ba955bafef007226
Javier de Santiago <j.desantiagosoto@gmail.com>
2026-07-09 12:29:02 +0200

    feat: add header and footer in Root

--- frontend/src/router/Root.tsx
+++ frontend/src/router/Root.tsx
@@ -1,9 +1,35 @@
 import { Outlet } from "react-router";
+import {useEffect} from "react";
+import AOS from "aos"
+import "aos/dist/aos.css";
+import Footer from "../shared/components/ui/Footer.tsx";
+import Header from "../shared/components/ui/Header.tsx";
 
 export default function Root() {
-  return (
-    <main id="app">
-      <Outlet />
-    </main>
-  );
+    useEffect(() => {
+        AOS.init({
+            once: true,
+            disable: "phone",
+            duration: 600,
+            easing: "ease-out-sine",
+        });
+    });
+
+    return (
+        <div
+            className="bg-brand-pitch-black-500 font-nacelle text-base text-brad-alabaster-grey-900 antialiased"
+        >
+            <div>
+                <header id="header">
+                    <Header />
+                </header>
+                <main id="app" className="relative flex grow flex-col">
+                    <Outlet />
+                </main>
+                <footer id="footer">
+                    <Footer />
+                </footer>
+            </div>
+        </div>
+    );
 }
