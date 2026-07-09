5978211cfc7ae98d1795ac0ce3ea0ad364ca25e7
Javier de Santiago <j.desantiagosoto@gmail.com>
2026-07-09 15:23:41 +0200

    refactor: update layouts styling

--- frontend/src/router/AuthLayout.tsx
+++ frontend/src/router/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function AuthLayout() {
     return (
-        <div className="flex min-h-screen items-center justify-center bg-brand-pitch-black-500 px-4">
+        <div className="flex min-h-screen items-center justify-center bg-brand-pitch-black-500 px-4 py-12">
             <Outlet />
         </div>
     );
