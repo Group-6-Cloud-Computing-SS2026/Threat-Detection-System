4725de5fc990e415633085d56ff2deffc690c44c
Javier de Santiago <j.desantiagosoto@gmail.com>
2026-07-09 12:15:51 +0200

    feat: add Logo component

---
+++ frontend/src/shared/components/ui/Logo.tsx
@@ -0,0 +1,9 @@
+import {NavLink} from "react-router";
+
+export default function Logo() {
+  return (
+    <NavLink to="/" className="inline-flex shrink-0 items-center" aria-label="ThreatOff" end>
+        <img src="/favicon.svg" alt="ThreatOff" width={32} height={32} className="h-8 w-8" />
+    </NavLink>
+  );
+}
