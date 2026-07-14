54517b53acebbf9cc4278c88a4a2e9c73c78e735
javierdesant <j.desantiagosoto@gmail.com>
2026-07-14 02:38:48 +0200

    feat: export authentication and utility hooks from shared/hooks directory

---
+++ frontend/src/shared/hooks/index.ts
@@ -0,0 +1,6 @@
+export { useAuthFields } from "./useAuthFields";
+export { useAuthMode } from "./useAuthMode";
+export { useAuthRequests } from "./useAuthRequests";
+export { usePanelHeight } from "./usePanelHeight";
+export { default as useMasonry } from "./useMasonry";
+export { default as useMousePosition } from "./useMousePosition";
