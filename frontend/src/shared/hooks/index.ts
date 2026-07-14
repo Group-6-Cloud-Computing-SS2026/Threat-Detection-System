baed60656aef9caf12a7d6dc75a3ebcf09654edd
javierdesant <j.desantiagosoto@gmail.com>
2026-07-14 04:17:43 +0200

    refactor: remove `useMasonry` hook

--- frontend/src/shared/hooks/index.ts
+++ frontend/src/shared/hooks/index.ts
@@ -2,5 +2,4 @@ export { useAuthFields } from "./useAuthFields";
 export { useAuthMode } from "./useAuthMode";
 export { useAuthRequests } from "./useAuthRequests";
 export { usePanelHeight } from "./usePanelHeight";
-export { default as useMasonry } from "./useMasonry";
 export { default as useMousePosition } from "./useMousePosition";
