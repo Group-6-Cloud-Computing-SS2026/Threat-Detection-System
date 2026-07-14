11529ef0dd62a6b8452ec559dcbf9b9c813cdb95
Javier de Santiago <j.desantiagosoto@gmail.com>
2026-07-14 23:24:29 +0200

    feat: improve operations page responsiveness

--- frontend/src/pages/home/operations/OperationsPage.tsx
+++ frontend/src/pages/home/operations/OperationsPage.tsx
@@ -194,7 +194,7 @@ export default function OperationsPage() {
         data-aos="fade-up"
         data-aos-delay={80}
       >
-        <div className="flex flex-wrap gap-2">
+        <div className="flex flex-wrap gap-2 justify-center md:justify-between">
           {NAV_ITEMS.map((item) => (
             <button
               key={item.id}
